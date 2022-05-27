5e9e4d1e9aa46e1b05300e2ecfa9b88e83e0fa93
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2022-05-26 22:19:39 -0400

    Update comments and launch screen

--- Magic 8 Ball/AppDelegate.swift
+++ Magic 8 Ball/AppDelegate.swift
@@ -1,8 +1,7 @@
-//
+//  Sabah Naveed
 //  AppDelegate.swift
 //  Magic 8 Ball
 //
-//  Created by Angela Yu on 14/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -1,8 +1,7 @@
-//
+//  Sabah Naveed
 //  ViewController.swift
 //  Magic 8 Ball
 //
-//  Created by Angela Yu on 14/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
@@ -13,6 +12,10 @@ class ViewController: UIViewController {
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
 }
 
